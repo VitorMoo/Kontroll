c2949b7bc99a72ab5318edfa166e7f2fbb235b1a
Vitor Moreira <viitornmoreira@outlook.com>
2025-04-27 02:36:49 -0300

    feat: Implement user registration view with form handling and success messages

--- core/views.py
+++ core/views.py
@@ -1,1 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.views import View
+from core.form import UserRegisterForm
+
+
+class UserRegisterView(View):
+    def get(self, request):
+        form = UserRegisterForm()
+        return render(request, 'core/register.html', {form : 'form'})
+
+    def post(self, request):
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully! You can now log in.')
+        return render(request, 'core/register.html', {'form': form})
