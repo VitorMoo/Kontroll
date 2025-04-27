793874cf4712eeba0277c8ceb76097164f004a8e
Vitor Moreira <viitornmoreira@outlook.com>
2025-04-27 00:20:42 -0300

    feat: Implement User and Account admin interfaces

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models.user import User
+from .models.account import Account
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display     = ('id', 'username', 'email', 'cpf',
+                        'telephone_number', 'is_active',
+                        'is_staff', 'created_at')
+    search_fields    = ('username', 'email', 'cpf', 'telephone_number')
+    list_filter      = ('is_active', 'is_staff')
+    ordering         = ('-created_at',)
+    list_per_page    = 25
+
+admin.site.register(User, UserAdmin)
+
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display     = ('id', 'user', 'name', 'balance', 'created_at')
+    search_fields    = ('name', 'user__username', 'user__email')
+    list_filter      = ('user__is_active',)
+    ordering         = ('-created_at',)
+    list_select_related = ('user',)
+    list_per_page    = 25
+
+
+admin.site.register(Account, AccountAdmin)
